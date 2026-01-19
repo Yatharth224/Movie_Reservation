93cbbebd9643f8789ec959da80c9f064cc3987d8
Your Name <yatharthchouhan22@gmail.com>
2026-01-19 21:10:14 +0530

    signup  route

--- app.py
+++ app.py
@@ -17,3 +17,20 @@ def landing():
     return render_template('landing.html')
 
 
+@app.route('/signup', methods=['GET','POST'])
+def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = bcrypt.generate_password_hash(
+            request.form['password']
+        ).decode('utf-8')
+
+        cur = mysql.connection.cursor()
+        cur.execute(
+            "INSERT INTO users (name,email,password) VALUES (%s,%s,%s)",
+            (name,email,password)
+        )
+        mysql.connection.commit()
+        return redirect('/login')
+    return render_template('signup.html')
