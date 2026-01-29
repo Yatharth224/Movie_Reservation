ddcd4660cc021cebea61d2f2b27a28ef6ecfc0e8
Your Name <yatharthchouhan22@gmail.com>
2026-01-29 18:04:51 +0530

    config

---
+++ config.py
@@ -0,0 +1,7 @@
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'yatharth'     # ya root (jo tumne banaya ho)
+MYSQL_PASSWORD = 'Rachit@224'  # jo password diya ho
+MYSQL_DB = 'movie_booking'
+
+SECRET_KEY = 'super-secret-key'
+SESSION_TYPE = 'filesystem'
